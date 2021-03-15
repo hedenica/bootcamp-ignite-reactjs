b443ab65ffd208c661f12cce83a9b363150f3fb5
hedenicapedrosa <hedenica@hotmail.com>
2021-03-15 18:50:34 -0300

    ✨  Listing repositories

---
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryItem({ repository }) {
+  return (
+    <li>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+
+      <a href={repository.html_url}>
+        Acessar repositório
+      </a>
+    </li>
+  )
+}
---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { RepositoryItem } from "./RepositoryItem"
+
+import '../styles/repositories.scss';
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        {repositories.map(repository => (
+          <RepositoryItem 
+            key={repository.id} 
+            repository={repository} 
+          />
+        ))}
+      </ul>
+    </section>
+  )
+}
