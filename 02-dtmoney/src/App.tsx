69ebf606ac76c867b9d4cc060ec82ccfbf1dff57
hedenicapedrosa <hedenica@hotmail.com>
2021-03-17 19:54:31 -0300

    ✨  add  header

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from './components/Header'
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
+    <>
+     <Header />
+     <GlobalStyle />
+    </>
   );
 }
