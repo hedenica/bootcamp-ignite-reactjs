ad4e6ac365bb162359a18ec51a34bd5e6fc5b5fc
hedenicapedrosa <hedenica@hotmail.com>
2021-04-01 19:43:43 -0300

    ✨  add _document

--- 03-ignews/src/pages/_app.tsx
+++ 03-ignews/src/pages/_app.tsx
@@ -5,3 +5,4 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp
+ 
--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
   )
 }
